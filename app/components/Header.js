58144ad61c01fc69dd978912d88716fa3fafc36a
mcmanyika <mcmanyika.com>
2023-06-21 16:39:37 -0500

    production

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,25 +3,25 @@
 function Header() {
   return (
     <div className=' bg-slate-900 bg-opacity-70'>
-          <div className='flex justify-between max-w-7xl mx-auto p-10 uppercase font-bold text-white text-center items-center'>
-            <div className=' w-1/3'>
+          <div className=' md:flex md:justify-between md:max-w-7xl md:mx-auto p-10 uppercase font-bold text-white text-center items-center'>
+            <div className=' md:w-1/3'>
                 <div className=" text-left text-3xl">Mireya</div>
                 <div className="text-left text-sm font-thin">Business solutions</div>
             </div>
-            <div className=''>
+            <div className='hidden md:flex'>
               About
             </div>
-            <div className=''>
+            <div className='hidden md:flex'>
               Services
             </div>
-            <div className=''>
+            <div className='hidden md:flex'>
               Contacts
             </div>
-            <div className=''>
+            <div className='hidden md:flex'>
               Our Team
             </div>
-            <div>
-                <button className=" border-2 border-white rounded-full p-2 pl-5 pr-5"> Get A Quote</button>
+            <div className="hidden md:flex">
+                <button className=" border-2 border-white rounded-full p-2 pl-5 pr-5 hover:bg-black hover:text-white"> Get A Quote</button>
             </div>
           </div>
         </div>
--- app/components/MiddleBanner.js
+++ app/components/MiddleBanner.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function MiddleBanner() {
   return (
     <div className='flex bg-yellow-400 p-16 mx-auto max-w-7xl justify-between items-center'>
-        <div className=' text-5xl text-white'>
+        <div className='text-lg md:text-5xl text-white'>
             We Have A Team <br /> Ready To Serve You
         </div>
-        <div>
+        <div className='text-lg'>
         <button className="border-2 border-white rounded-full pl-4 pr-4 p-2">
         Discover More {' >'}
         </button>
