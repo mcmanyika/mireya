e3fe3253b36227b3f9a778d8f6463c9d14ace85a
mcmanyika <mcmanyika.com>
2023-07-15 15:02:46 -0400

    join

--- app/components/Join.js
+++ app/components/Join.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Join() {
   return (
     <div className='md:p-10'>
-        <div className='w-1/2 mx-auto text-center'>
+        <div className='md:w-1/2 mx-auto text-center'>
             <div className='w-full col-auto text-5xl font-extrabold'>
                 Join the team
             </div>
